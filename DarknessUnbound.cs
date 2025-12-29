24f64b0201aae0bde14f39ace1058106f61bfc33
onejlong <onejlong@gmail.com>
2025-12-29 10:41:42 +0100

    test 2

--- DarknessUnbound.cs
+++ DarknessUnbound.cs
@@ -7,10 +7,13 @@ using Terraria.ModLoader;
 
 namespace DarknessUnbound
 {
-	//new comment test
+	//new comment test 2
 	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
 	public class DarknessUnbound : Mod
 	{
-
+		for (int i = 0; i < length; i++)
+		{
+			
+		}
 	}
 }
