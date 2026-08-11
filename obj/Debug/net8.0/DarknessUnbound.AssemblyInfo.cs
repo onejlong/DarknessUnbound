c2aac4f6515cb41f7a47d35bf09cc4d9c6ca11bf
onejlong <onejlong@gmail.com>
2026-08-11 17:20:28 +0200

    removed bad and gay text file

--- obj/Debug/net8.0/DarknessUnbound.AssemblyInfo.cs
+++ obj/Debug/net8.0/DarknessUnbound.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DarknessUnbound")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d83b5f356b474cf2c8c735e28bc78fc05fceb10c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6bd7a5f40550295e41dfb15213e13c32dd5cd73")]
 [assembly: System.Reflection.AssemblyProductAttribute("DarknessUnbound")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DarknessUnbound")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
